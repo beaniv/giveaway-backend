75ab9ec9fe47fbfc8c52f113b6ec78ce6dd75ced
Silence1337 <007.99@mail.ru>
2021-05-08 16:18:42 +0700

    add dependencies & main

---
+++ src/main/java/com/beaniv/giveaway/GiveawayApplication.java
@@ -0,0 +1,12 @@
+package com.beaniv.giveaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiveawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiveawayApplication.class, args);
+    }
+}
