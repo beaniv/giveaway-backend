168a8ac557cf6c7b34dc07a5705e477343acfe6e
Silence1337 <007.99@mail.ru>
2021-05-08 16:38:46 +0700

    add configs

---
+++ src/main/java/com/beaniv/giveaway/config/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.beaniv.giveaway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import com.beaniv.giveaway.authentication.security.jwt.JwtConfigurer;
+import com.beaniv.giveaway.authentication.security.jwt.JwtTokenProvider;
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String REGISTRATION_ENDPOINT = "/api/v1/auth/sign-up";
+    private static final String AUTHENTICATION_ENDPOINT = "/api/v1/auth/sign-in";
+    private static final String[] SWAGGER_WHITELIST = {
+            "/swagger-resources/**",
+            "/swagger-ui.html",
+            "/v2/api-docs",
+            "/webjars/**"
+    };
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .csrf()
+                    .disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .authorizeRequests()
+                    .antMatchers(REGISTRATION_ENDPOINT).permitAll()
+                    .antMatchers(AUTHENTICATION_ENDPOINT).permitAll()
+                    .antMatchers(SWAGGER_WHITELIST).permitAll()
+                    .antMatchers("/api/v1/**").permitAll()
+                    .antMatchers("/restoreSuccess.html").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                    .apply(new JwtConfigurer(jwtTokenProvider));
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:8443"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
+                "Cache-Control", "Content-Type"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ src/main/java/com/beaniv/giveaway/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.beaniv.giveaway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    private static final String SWAGGER_API_VERSION = "1.0";
+    private static final String LICENSE = "Apache License";
+    private static final String TITLE = "GIVEAWAY REST API";
+    private static final String DESCRIPTION = "RESTful API for GIVEAWAY";
+
+    @Bean
+    public Docket beanivApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.beaniv.giveaway"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(TITLE)
+                .description(DESCRIPTION)
+                .license(LICENSE)
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                .version(SWAGGER_API_VERSION)
+                .build();
+    }
+}
